f63c1990b9a5e246820bd1aa4dd946e6a8e6e5be
harshitadahiya9 <harshitadahiya1456@gmail.com>
2025-11-24 13:01:40 +0530

    Add programs to count even/odd and pos/neg/zero

---
+++ day30.c
@@ -0,0 +1,58 @@
+Q59: 
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int a[100];
+    int even = 0, odd = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for(i = 0; i < n; i++) {
+        if(a[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Even = %d, Odd = %d\n", even, odd);
+
+    return 0;
+}
+Q60: 
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int a[100];
+    int pos = 0, neg = 0, zero = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for(i = 0; i < n; i++) {
+        if(a[i] > 0)
+            pos++;
+        else if(a[i] < 0)
+            neg++;
+        else
+            zero++;
+    }
+
+    printf("Positive = %d, Negative = %d, Zero = %d\n", pos, neg, zero);
+
+    return 0;
+}
