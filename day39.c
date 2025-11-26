7034d1a8d5e6cc3235d620f1634e31eb9faf7594
harshitadahiya9 <harshitadahiya1456@gmail.com>
2025-11-26 15:38:01 +0530

    Add distinctness check and diagonal sum for matrices
    Implement matrix distinctness check and diagonal sum calculation.

---
+++ day39.c
@@ -0,0 +1,70 @@
+Q77: 
+
+#include <stdio.h>
+
+int main() {
+    int r, c, i, j, k, distinct = 1;
+    int a[10][10];
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    
+    if(r != c) {
+        printf("False\n");
+        return 0;
+    }
+
+    printf("Enter elements of the matrix:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    
+    for(i = 0; i < r; i++) {
+        for(k = i + 1; k < r; k++) {
+            if(a[i][i] == a[k][k]) {
+                distinct = 0;
+                break;
+            }
+        }
+        if(distinct == 0)
+            break;
+    }
+
+    if(distinct == 1)
+        printf("True\n");
+    else
+        printf("False\n");
+
+    return 0;
+}
+Q78: 
+
+#include <stdio.h>
+
+int main() {
+    int n, i, j, sum = 0;
+    int a[10][10];
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &n, &n);
+
+    printf("Enter elements of the matrix:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    
+    for(i = 0; i < n; i++) {
+        sum += a[i][i];
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
