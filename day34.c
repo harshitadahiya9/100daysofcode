1a16f754f5f736f4277a88af79450e61994e4499
harshitadahiya9 <harshitadahiya1456@gmail.com>
2025-11-26 15:19:14 +0530

    Implement array insertion and deletion operations

---
+++ day34.c
@@ -0,0 +1,68 @@
+Q67: 
+
+#include <stdio.h>
+
+int main() {
+    int n, pos, num, i;
+    int a[100];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter position and element to insert: ");
+    scanf("%d %d", &pos, &num);
+
+    
+    for(i = n - 1; i >= pos; i--) {
+        a[i + 1] = a[i];
+    }
+
+    
+    a[pos] = num;
+    n++; 
+
+    printf("Array after insertion:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
+Q68: 
+
+#include <stdio.h>
+
+int main() {
+    int n, pos, i;
+    int a[100];
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter position to delete: ");
+    scanf("%d", &pos);
+
+    
+    for(i = pos; i < n - 1; i++) {
+        a[i] = a[i + 1];
+    }
+
+    n--; 
+
+    printf("Array after deletion:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
