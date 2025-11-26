e1d52656ed56e33c2240cd8a7ca99011ef41e06f
harshitadahiya9 <harshitadahiya1456@gmail.com>
2025-11-26 20:11:08 +0530

    Create day82.c

---
+++ day82.c
@@ -0,0 +1,22 @@
+Q132: 
+#include <stdio.h>
+
+enum TrafficLight {
+    RED,
+    YELLOW,
+    GREEN
+};
+
+int main() {
+    enum TrafficLight signal = GREEN;
+
+    if (signal == RED) {
+        printf("Stop\n");
+    } else if (signal == YELLOW) {
+        printf("Wait\n");
+    } else if (signal == GREEN) {
+        printf("Go\n");
+    }
+
+    return 0;
+}
