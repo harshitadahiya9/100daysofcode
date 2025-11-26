bec5f69b79fb6d385bba7cae3b1b251fc8e4e8a6
harshitadahiya9 <harshitadahiya1456@gmail.com>
2025-11-26 19:57:14 +0530

    Create day58.c

---
+++ day58.c
@@ -0,0 +1,30 @@
+Q108:  
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int nums[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &nums[i]);
+
+    int answer[n];
+    for (int i = 0; i < n; i++) {
+        int prod = 1;
+        for (int j = 0; j < n; j++) {
+            if (i != j)
+                prod *= nums[j];
+        }
+        answer[i] = prod;
+    }
+
+    printf("[");
+    for (int i = 0; i < n; i++) {
+        printf("%d", answer[i]);
+        if (i != n - 1)
+            printf(",");
+    }
+    printf("]");
+
+    return 0;
+}
