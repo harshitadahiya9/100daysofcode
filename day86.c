6898aaf2f2659769c8b9602e14a1027b205e481c
harshitadahiya9 <harshitadahiya1456@gmail.com>
2025-11-26 20:14:14 +0530

    Create day86.c

---
+++ day86.c
@@ -0,0 +1,27 @@
+Q136: 
+#include <stdio.h>
+
+enum Menu {
+    ADD,
+    SUBTRACT,
+    MULTIPLY
+};
+
+int main() {
+    enum Menu choice = ADD;
+    int a = 10, b = 20;
+
+    switch (choice) {
+        case ADD:
+            printf("%d\n", a + b);
+            break;
+        case SUBTRACT:
+            printf("%d\n", a - b);
+            break;
+        case MULTIPLY:
+            printf("%d\n", a * b);
+            break;
+    }
+
+    return 0;
+}
